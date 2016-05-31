f85536f367d1133223f6b2910732aa5bce9696c5
j-test <sx.work@outlook.com>
2016-06-01 00:41:35 +0800

    v1.0

--- conf.py
+++ conf.py
@@ -120,7 +120,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -130,11 +131,12 @@ html_theme = 'alabaster'
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
+html_theme_path = ['.']
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
 #
-# html_title = 'Test-Cookbook vv1.0'
+html_title = 'Test-Cookbook v1.0'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #
